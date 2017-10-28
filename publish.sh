e4d082d8e2d48d5dc8f9b77f60fcb8dc63a74957
Mark Woodhall <mark.woodhall@gmail.com>
2017-10-28 21:00:03 +0100

    Another script tweak

--- publish.sh
+++ publish.sh
@@ -7,6 +7,6 @@ cp -r ../public/* ./
 git add .
 git commit . -m "Publish"
 git push origin deploy
-git push deploy deploy
-git push deploy2 deploy
+git push deploy deploy:master
+git push deploy2 deploy:master
 rm -rf ../public
