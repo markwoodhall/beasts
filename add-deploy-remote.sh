87753519fe11864efc22137e07f9b8920ca5b3e3
Mark Woodhall <mark.woodhall@gmail.com>
2017-10-29 12:25:01 +0000

    Clean gh-pages branch

--- add-deploy-remote.sh
+++
@@ -1,5 +0,0 @@
-git remote remove deploy
-git remote add deploy dokku@mwoodhall.com:beasts.mwoodhall.com
-
-git remote remove deploy2
-git remote add deploy2 dokku@markwoodhall.com:beasts.markwoodhall.com
